85fccb4f3fb8088e282ec7594dcd622a2d6effb6
Dipchand kalwar <111203713+dipchandkalwar@users.noreply.github.com>
2025-03-26 04:43:48 +0530

    Delete Project directory

--- Project/project3/chaiaurcode.js
+++
@@ -1,8 +0,0 @@
-//const clock=document.querySelector("#clock");
-const clock=document.getElementById('clock');
-//1000=1sec
-setInterval(function(){
-
-  let date=new Date();
-  clock.innerHTML=date.toLocaleTimeString();
-},1000)
