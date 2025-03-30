b95e3ba466d0439706bca548ec5413005d60b6c0
Dipchand kalwar <111203713+dipchandkalwar@users.noreply.github.com>
2025-03-30 10:23:01 +0000

    promises

--- 09_advance_one/promise.js
+++ 09_advance_one/promise.js
@@ -36,27 +36,80 @@
 // })
 
 
-const promiseFour=new Promise(function(resolve,reject){
-    setTimeout(function(){
-        // let error=false
-        let error=true
-        if(!error){
-            resolve({username:"dip",password:"123"})
-        }
-        else{
-            reject('ERROR: something went wrong')
-        }
-    },1000)
+// const promiseFour=new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         // let error=false
+//         let error=true
+//         if(!error){
+//             resolve({username:"dip",password:"123"})
+//         }
+//         else{
+//             reject('ERROR: something went wrong')
+//         }
+//     },1000)
 
-})
+// })
 
-promiseFour.then((user)=>{
-console.log(user);
-return user.username
-}).then((username)=>{
-console.log(username)
-}).catch(function(error){
-    console.log(error);
-}).finally(()=>{
-    console.log("the promise is either resolved or rejected");
+// promiseFour.then((user)=>{
+// console.log(user);
+// return user.username
+// }).then((username)=>{
+// console.log(username)
+// }).catch(function(error){
+//     console.log(error);
+// }).finally(()=>{
+//     console.log("the promise is either resolved or rejected");
+// })
+
+
+// const PromiseFive=new Promise(function(resolve,reject){
+
+//     setTimeout(function(){
+//         // let error=false
+//         let  error=true
+//         if(!error){
+//             resolve({username:"javascript",password:"123"})
+//         }
+//         else{
+//             reject('ERROR: js went wrong')
+//         }
+//     },1000)
+// })
+
+// async function consumePromiseFive() {
+//    try{
+//     const response=await PromiseFive 
+//    console.log(response);
+//    }
+//    catch(error){
+//     console.log(error);
+//    }
+// }
+
+// consumePromiseFive();
+
+// //try and catch format
+// async function getAllUser(){
+//   try {
+//     const response=await fetch('https://api.github.com/users/hiteshchoudhary')
+    
+//     const data= await response.json();
+//     console.log(data);
+//   } catch (error) {
+//     console.log("E. ",error);
+//   }
+// } 
+
+// getAllUser();
+
+//then/catch format
+
+
+fetch('https://api.github.com/users/hiteshchoudhary').then((response)=>{
+return response.json()
 })
+.then((data)=>{
+    console.log(data);
+})
+.catch((error)=>console.log(error))
+    
